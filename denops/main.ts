cd262e0a157904a79bd0b60878269839b24aaa35
Cassin01 <koyojb@gmail.com>
2022-05-01 00:59:54 +0900

    hoge

--- denops/main.ts
+++ denops/main.ts
@@ -1,11 +1,9 @@
-import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
-import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async echo(text: unknown): Promise<unknown> {
-      ensureString(text);
-      return await Promise.resolve(text);
-    },
-  };
+      hello() {
+          return Promise.resolve("Hello");
+      }
+  }
 };
