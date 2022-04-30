fe905d9991f68f565a6b840d77e54487aef2583d
Cassin01 <koyojb@gmail.com>
2022-05-01 02:34:46 +0900

    a

--- denops/fetch-info/main.ts
+++ denops/fetch-info/main.ts
@@ -1,20 +1,19 @@
 import { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
 
 
-  export async function main(denops: Denops): Promise<void> {
+export async function main(denops: Denops): Promise<void> {
   const city = await fetch('https://ipinfo.io/city');
-    const city_text = await city.text();
+  const city_text = await city.text();
   denops.dispatcher = {
-    async c () { await fetch('http://wttr.in/'.concat(city_text, '?format=%c'))},
-    async m () { await fetch('http://wttr.in/'.concat(city_text, '?format=%m'))},
-    async f () { await fetch('http://wttr.in/'.concat(city_text, '?format=%f'))},
-    async w () { await fetch('http://wttr.in/'.concat(city_text, '?format=%w'))},
+    async c() { await fetch('http://wttr.in/'.concat(city_text, '?format=%c'))},
+    async m() { await fetch('http://wttr.in/'.concat(city_text, '?format=%m'))},
+    async f() { await fetch('http://wttr.in/'.concat(city_text, '?format=%f'))},
+    async w() { await fetch('http://wttr.in/'.concat(city_text, '?format=%w'))},
   }
-  // await denops.cmd(`function! Meow() echom "Meow! endfunction"`);
 };
 
 /**
   * INFO
-* call denops#notify("fetch-info", "hello", [])
+* call denops#notify("fetch-info", "f", [])
 * lua print(vim.inspect(vim.api.nvim_exec([[call denops#request_async("fetch-info", "wttr", [], )]], true)))
 */
