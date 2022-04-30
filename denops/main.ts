555ea9ca2661455eb3a96e90b1fd3eb18803a4d2
Cassin01 <koyojb@gmail.com>
2022-05-01 00:51:27 +0900

    first commit

---
+++ denops/main.ts
@@ -0,0 +1,11 @@
+import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
+import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+
+export async function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<unknown> {
+      ensureString(text);
+      return await Promise.resolve(text);
+    },
+  };
+};
